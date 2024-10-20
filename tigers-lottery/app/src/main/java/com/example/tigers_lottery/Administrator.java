a01fbad6068e47f260155bc3894ebd26695a3c3a
abdeahmat <ahmatabderahman501@gmail.com>
2024-10-20 14:59:01 -0600

    Updated the basic information for the major classes

--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/Administrator.java
+++ tigers-lottery/app/src/main/java/com/example/tigers_lottery/Administrator.java
@@ -1,7 +1,8 @@
 package com.example.tigers_lottery;
 
 public class Administrator extends User {
-    public Administrator(String email, String password) {
-        super(email, password);
+
+    public Administrator(String email, String password, String firstName, String lastName, String userName) {
+        super(email, password, firstName, lastName, userName);
     }
 }
--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/User.java
+++ tigers-lottery/app/src/main/java/com/example/tigers_lottery/User.java
@@ -3,10 +3,16 @@ package com.example.tigers_lottery;
 public class User {
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String userName;
     private String number;
-    public User (String email, String password){
+    public User (String email, String password, String firstName, String lastName, String userName){
         this.email=email;
         this.password=password;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.userName=userName;
     }
 
     public void setEmail(String email){
@@ -17,8 +23,32 @@ public class User {
 
     }
 
+    public void setFirstName(String firstName){
+
+    }
+
+    public void setLastName(String lastName){
+
+    }
+
+    public void setUserName(String userName){
+
+    }
+
     public void setNumber(String number){
 
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getUserName(){
+        return this.userName;
     }
 
     public String getEmail(){
