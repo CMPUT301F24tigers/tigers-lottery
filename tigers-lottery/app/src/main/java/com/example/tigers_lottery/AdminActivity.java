389f2c0433fa56a795b77736b68867939139d50a
Chathila <ratnatil@ualberta.ca>
2024-10-30 10:38:21 -0600

    testing branch rule set

--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/AdminActivity.java
+++ tigers-lottery/app/src/main/java/com/example/tigers_lottery/AdminActivity.java
@@ -6,7 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminActivity extends AppCompatActivity {
-
+    //adding some comments
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
