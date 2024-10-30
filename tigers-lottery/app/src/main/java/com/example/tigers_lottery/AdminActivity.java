f0083a2a65245402cb0c7ac33d09f1bd490845c3
Chathila <ratnatil@ualberta.ca>
2024-10-30 10:46:12 -0600

    adding some test comments

--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/AdminActivity.java
+++ tigers-lottery/app/src/main/java/com/example/tigers_lottery/AdminActivity.java
@@ -6,7 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminActivity extends AppCompatActivity {
-
+    //comments
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
