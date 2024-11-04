07957c3f44b284a4a51216c8ef4cc59c32275af9
pranavpenmetsa <nagapran@ualberta.ca>
2024-11-04 14:54:05 -0700

    -Created 3 organizer test files

--- tigers-lottery/app/src/androidTest/java/com/example/tigers_lottery/OrganizerEventDetailsFragmentTest.java
+++ tigers-lottery/app/src/androidTest/java/com/example/tigers_lottery/OrganizerEventDetailsFragmentTest.java
@@ -1,37 +1,46 @@
 package com.example.tigers_lottery;
 
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+
 import com.example.tigers_lottery.HostedEvents.OrganizerEventDetailsFragment;
 import com.example.tigers_lottery.MainActivity;
 import com.example.tigers_lottery.R;
+
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertTrue;
 
+
 @RunWith(AndroidJUnit4.class)
 public class OrganizerEventDetailsFragmentTest {
+
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
+
     private FragmentManager fragmentManager;
+
 
     @Before
     public void setUp() {
         ActivityScenario<MainActivity> scenario = activityScenarioRule.getScenario();
         scenario.onActivity(activity -> fragmentManager = activity.getSupportFragmentManager());
     }
+
 
     @Test
     public void testOrganizerEventDetailsFragmentDisplayed() {
@@ -42,9 +51,11 @@ public class OrganizerEventDetailsFragmentTest {
                     .replace(R.id.main_activity_fragment_container, fragment)
                     .commitNow();
 
+
             // Check if OrganizerEventDetailsFragment is displayed  c
             Fragment currentFragment = fragmentManager.findFragmentById(R.id.main_activity_fragment_container);
             assertTrue(currentFragment instanceof OrganizerEventDetailsFragment);
         });
     }
 }
+
--- tigers-lottery/app/src/androidTest/java/com/example/tigers_lottery/OrganizerQRCodeFragmentTest.java
+++ tigers-lottery/app/src/androidTest/java/com/example/tigers_lottery/OrganizerQRCodeFragmentTest.java
@@ -1,12 +1,16 @@
 package com.example.tigers_lottery;
 
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+
 import com.example.tigers_lottery.HostedEvents.OrganizerQRCodeFragment;
+
+
 
 
 import org.junit.Before;
@@ -14,25 +18,31 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertTrue;
 
+
 @RunWith(AndroidJUnit4.class)
 public class OrganizerQRCodeFragmentTest {
+
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
+
     private FragmentManager fragmentManager;
+
 
     @Before
     public void setUp() {
         ActivityScenario<MainActivity> scenario = activityScenarioRule.getScenario();
         scenario.onActivity(activity -> fragmentManager = activity.getSupportFragmentManager());
     }
+
 
     @Test
     public void testOrganizerQRCodeFragmentDisplayed() {
@@ -43,10 +53,12 @@ public class OrganizerQRCodeFragmentTest {
                     .replace(R.id.main_activity_fragment_container, fragment)
                     .commitNow();
 
+
             // Check if OrganizerQRCodeFragment is displayed c
             Fragment currentFragment = fragmentManager.findFragmentById(R.id.main_activity_fragment_container);
             assertTrue(currentFragment instanceof OrganizerQRCodeFragment);
         });
+
 
         // Verify the button and image are displayed
         onView(withId(R.id.qrCodeRegenerateButton)).check(matches(isDisplayed()));
@@ -54,4 +66,7 @@ public class OrganizerQRCodeFragmentTest {
     }
 
 
+
+
 }
+
