67639eee971bcfc9b735d9ce4354551cfbeb0eb2
abdeahmat <ahmatabderahman501@gmail.com>
2024-11-05 22:55:25 -0700

    Deleted unnecessary classes, started base test for create event.

--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/HostedEvents/OrganizerCreateProfileFragment.java
+++
@@ -1,8 +0,0 @@
-package com.example.tigers_lottery.HostedEvents;
-
-/**
- * Unused.
- */
-
-public class OrganizerCreateProfileFragment {
-}
--- tigers-lottery/app/src/main/java/com/example/tigers_lottery/models/Entrant.java
+++
@@ -1,9 +0,0 @@
-package com.example.tigers_lottery.models;
-
-
-public class Entrant extends User {
-
-    public Entrant(String email, String password, String firstName, String lastName, String userName) {
-        //super(email, password, firstName, lastName, userName);
-    }
-}
